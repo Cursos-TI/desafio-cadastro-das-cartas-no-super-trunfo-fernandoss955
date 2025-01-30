533c4786bd38d458ddf0cb8fde80c51e5d307789
fernandoss955 <fernandosilvacage@gmail.com>
2025-01-30 12:34:09 -0300

    Atualizar o Supetrunfo Pais.c
    novo prinf

--- Supetrunfo Pais.c
+++ Supetrunfo Pais.c
@@ -5,7 +5,9 @@ int main(){
     printf("Codigo - A01 \n");
     printf("Pais - BRASIL \n");
     printf("Estado - Sao paulo \n");
-
+    printf("população \n");
+    printf("População Por kl² \n");
+    
 
      return 0;
 
